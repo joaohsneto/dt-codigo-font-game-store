2f3852b59e14647b267aec2ee523661b761b16ef
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-12-21 04:33:23 -0300

    Criado na pasta pages a página Home.jsx

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/products.css';
+
+import ListProducts from '../components/ListProducts';
+
+const Home = () => {
+
+  return (
+    <>
+      <header>
+        <h1>Navbar aqui</h1>
+      </header>
+      <ListProducts />
+    </>
+  );
+};
+
+export default Home;
