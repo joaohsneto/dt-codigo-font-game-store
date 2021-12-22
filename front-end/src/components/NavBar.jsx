72d09aa2f91c39821dff20f6b7187c99f0b07918
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-12-22 04:08:38 -0300

    Feito alteraçãoes no arquivo Navbar

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const GeneralNavbar = () => {
 
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
+    <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt='bulma-logo' />
+        <a className="navbar-item" href="/">
+          OVER GAMES
         </a>
 
-        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="true" data-target="navbarBasicExample">
+        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="true" data-target="navbarBasicExample" href='/'>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -18,7 +18,7 @@ const GeneralNavbar = () => {
 
       <div id="navbarBasicExample" className="navbar-menu">
         <div className="navbar-start">
-          <a className="navbar-item">
+          <a className="navbar-item" href='/'>
             Home
           </a>
         </div>
@@ -26,10 +26,10 @@ const GeneralNavbar = () => {
         <div className="navbar-end">
           <div className="navbar-item">
             <div className="buttons">
-              <a className="button is-primary">
+              <a className="button is-primary" href='/'>
                 <strong>Cadastrar</strong>
               </a>
-              <a className="button is-light">
+              <a className="button is-light" href='/'>
                 Entrar
               </a>
             </div>
