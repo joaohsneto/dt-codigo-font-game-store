96d9b836778edff669c7532511fda34e79884308
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-12-23 02:49:46 -0300

    gerando usuário com token no services createUser

--- back-end/services/userServices.js
+++ back-end/services/userServices.js
@@ -1,8 +1,11 @@
 const model = require('../models/userModels');
+const { createToken } = require('../middleware/tokenGeneration');
 
 const createUser = async ({email, password}) => {
   const create = await model.createUser({email, password});
-  return create;
+  const token = createToken(create);
+  const userWithToken = { ...create, token};
+  return userWithToken;
 };
 
 module.exports = {
